e96c159f87eaf5cbb8206bcc2f249f5222233ea5
jhumbrac <john.humbracht@generalassemb.ly>
2020-12-21 09:22:44 -0600

    Updates

---
+++ precache-manifest.b156a911cc67a48c279f3325e90d6263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ee027d06903613af5b6",
+    "url": "/GameOfPrompts/static/css/main.5e3f2d3d.chunk.css"
+  },
+  {
+    "revision": "7ee027d06903613af5b6",
+    "url": "/GameOfPrompts/static/js/main.70351d65.chunk.js"
+  },
+  {
+    "revision": "4f8f6fbe084ed8458ef1",
+    "url": "/GameOfPrompts/static/js/runtime~main.15257d37.js"
+  },
+  {
+    "revision": "f9368d7482202b578a98",
+    "url": "/GameOfPrompts/static/js/2.cfdcccea.chunk.js"
+  },
+  {
+    "revision": "332b0945b1fa2410b01c2b267160d7ed",
+    "url": "/GameOfPrompts/static/media/bg.332b0945.jpg"
+  },
+  {
+    "revision": "1eafaff3587461b5c0e53f90ebabd2f6",
+    "url": "/GameOfPrompts/index.html"
+  }
+];
